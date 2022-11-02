b32dcc38aa3a9c8d27134ade9a36d5f78de1d175
Diego <diegoulloao@icloud.com>
2022-11-02 02:54:40 -0300

    fix: add pathname to duplicate tab names

--- plugins/bufferline.lua
+++ plugins/bufferline.lua
@@ -7,12 +7,11 @@ end
 -- custom setup
 bufferline.setup({
 	options = {
-		mode = "tabs",
-		numbers = "ordinal",
-		-- nvim lsp diagnostics integration in tabs
-		diagnostics = "nvim_lsp",
-		-- diagnostics format
-		diagnostics_indicator = function(count, level)
+		mode = "tabs", -- only show tabs and not all buffers
+		numbers = "ordinal", -- add tabs ordinal numbers
+		show_duplicate_prefix = true, -- show base path if tabs have the same name
+		diagnostics = "nvim_lsp", -- nvim lsp diagnostics integration in tabs
+		diagnostics_indicator = function(count, level) -- diagnostics format
 			local icon = level:match("error") and " " or " "
 			return " " .. icon .. count
 		end,
