8f8260449b2aa8e1da63442202e1088667b1c60d
Diego <diegoulloao@icloud.com>
2022-11-03 00:45:42 -0300

    fix: telescope git keymaps, minor keymaps file changes

--- core/keymaps.lua
+++ core/keymaps.lua
@@ -10,12 +10,14 @@ keymap.set("n", "<leader>q", "<cmd>q<CR>") -- quit from buffer
 keymap.set("n", "<leader>Q", "<cmd>q!<CR>") -- force quit from buffer
 keymap.set("n", "<leader>i", "<C-W><C-R>") -- invert panes
 keymap.set("n", "--", "<C-W>=") -- equals panes
-keymap.set("n", "<leader>rr", "<cmd>source $MYVIMRC<CR>") -- reload neovim config
 keymap.set("n", "x", '"_x') -- delete character without copy to clipboard
 keymap.set("n", "<leader><CR>", ":nohl<CR>") -- clean status line
 keymap.set("n", "<leader>rr", "<cmd>luafile %<CR>") -- reload lua configuration
 
--- tabs navigation
+-- nvim-tree keymaps
+keymap.set("n", "<leader>nt", "<cmd>NvimTreeToggle<CR>") -- nvim tree toggle
+
+-- tabs navigation keymaps
 keymap.set("n", "<leader>j", "<cmd>tabp<CR>") -- go to previous tab
 keymap.set("n", "<leader>k", "<cmd>tabn<CR>") -- go to next tab
 -- go to tab [1-5]
@@ -35,8 +37,8 @@ keymap.set("n", "<C-g>", "<cmd>Telescope live_grep<CR>") -- find word in project
 keymap.set("n", "<C-b>", "<cmd>Telescope buffers<CR>") -- buffers list
 keymap.set("n", "<C-f>", "<cmd>Telescope current_buffer_fuzzy_find<CR>") -- find in buffer
 keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<CR>") -- git status
-keymap.set("n", "<leader>gc", "<cmd>Telescope git_commit<CR>") -- git commit
-keymap.set("n", "<leader>gh", "<cmd>Telescope git_bcommit<CR>") -- git commit for current buffer
+keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<CR>") -- git commit
+keymap.set("n", "<leader>gh", "<cmd>Telescope git_bcommits<CR>") -- git commit for current buffer
 keymap.set("n", "<leader>gB", "<cmd>Telescope git_branches<CR>") -- git branches
 
 -- git signs keymaps
@@ -53,4 +55,3 @@ keymap.set("n", "<leader>l", "<cmd>IndentBlanklineToggle<CR>") -- toggle indent 
 -- util keymaps
 keymap.set("n", "<leader>L", '<cmd>let @*=join([expand("%"), line(".")], ":")<CR><cmd>nohl<CR>') -- copy buffer path and current line
 keymap.set("n", "<leader>ms", "<cmd>MaximizerToggle<CR>") -- maximize and restore current buffer
-keymap.set("n", "<leader>nt", "<cmd>NvimTreeToggle<CR>") -- nvim tree toggle
