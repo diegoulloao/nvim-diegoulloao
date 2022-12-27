10490f5d522b708d495add9cf72e183819871aae
Diego <diegoulloao@icloud.com>
2022-12-26 23:06:51 -0300

    fix: telescope improved colors

--- core/colorscheme.lua
+++ core/colorscheme.lua
@@ -5,7 +5,7 @@ if not status then
 end
 
 -- statusline msg area text color
-vim.cmd([[ hi MsgArea guifg=#7c8f8f ]])
+vim.cmd([[ hi! MsgArea guifg=#7c8f8f ]])
 
 -- nvim-cmp custom colors
 vim.cmd([[ hi! BorderBG guibg=NONE guifg=#3EFFDC ]])
@@ -14,8 +14,8 @@ vim.cmd([[ hi! CursorLineBG gui=bold guibg=#3EFFDC guifg=#000000 ]])
 -- telescope custom colors
 vim.cmd([[ hi! TelescopeNormal guifg=#acb4c2 ]])
 vim.cmd([[ hi! TelescopePromptBorder guifg=#65D1FF ]])
-vim.cmd([[ hi! TelescopeSelection gui=bold guibg=#3EFFDC guifg=#000000 ]])
-vim.cmd([[ hi! TelescopeSelectionCaret guifg=#c792ea ]])
-vim.cmd([[ hi! TelescopeMatching gui=bold guifg=#c792ea ]])
+vim.cmd([[ hi! TelescopeSelection guibg=#65D1FF guifg=#000000 ]])
+vim.cmd([[ hi! TelescopeSelectionCaret guifg=#ae81ff ]])
+vim.cmd([[ hi! TelescopeMatching gui=bold guifg=#ae81ff ]])
 vim.cmd([[ hi! TelescopePromptTitle guibg=#c792ea guifg=#000000 ]])
-vim.cmd([[ hi! TelescopeResultsTitle guibg=#3EFFDC guifg=#000000 ]])
+vim.cmd([[ hi! TelescopeResultsTitle guibg=#65D1FF guifg=#000000 ]])
