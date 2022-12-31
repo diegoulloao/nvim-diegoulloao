be740b62c5dd2dc3e18bf448295203da7b1b0001
Diego <diegoulloao@icloud.com>
2022-12-31 02:54:45 -0300

    fix: dashboard

--- plugins/dashboard.lua
+++ plugins/dashboard.lua
@@ -10,39 +10,39 @@ local home = os.getenv("HOME")
 -- custom config
 dashboard.custom_center = {
   {
-    icon = "   ",
-    desc = "Recently latest session                  ",
-    shortcut = "SPC s l",
-    action = "SessionLoad",
-  },
-  {
-    icon = "  ",
-    desc = "Recently opened files                   ",
-    action = "DashboardFindHistory",
-    shortcut = "SPC f h",
-  },
-  {
     icon = "  ",
-    desc = "Find  File                              ",
+    desc = "Find File                               ",
     action = "Telescope find_files find_command=rg,--hidden,--files",
     shortcut = "SPC f f",
   },
   {
     icon = "  ",
     desc = "File Browser                            ",
-    action = "Telescope file_browser",
+    action = "NvimTreeOpen",
     shortcut = "SPC f b",
   },
   {
     icon = "  ",
-    desc = "Find  word                              ",
+    desc = "Find word                               ",
     action = "Telescope live_grep",
     shortcut = "SPC f w",
   },
   {
+    icon = "  ",
+    desc = "Recently opened files                   ",
+    action = "Telescope oldfiles",
+    shortcut = "SPC f h",
+  },
+  {
+    icon = "   ",
+    desc = "Recently latest session                 ",
+    shortcut = "SPC s l",
+    action = "SessionLoad",
+  },
+  {
     icon = "  ",
     desc = "Open Personal dotfiles                  ",
-    action = "Telescope dotfiles path=" .. home .. "/.dotfiles",
+    action = "edit ~/.config/nvim/lua/diegoulloao",
     shortcut = "SPC f d",
   },
 }
