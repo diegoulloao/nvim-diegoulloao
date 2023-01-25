989c56178e5808a4bc190e682fa320d3dc44417c
Diego <diegoulloao@icloud.com>
2023-01-25 16:47:14 -0300

    fix(dashboard): new config due updates on plugin

--- plugins/dashboard.lua
+++ plugins/dashboard.lua
@@ -5,41 +5,11 @@ if not status then
 end
 
 -- custom config
-dashboard.custom_center = {
-  {
-    icon = "  ",
-    desc = "Recent projects                         ",
-    shortcut = "SPC s l",
-    action = "Telescope projects",
+dashboard.setup({
+  config = {
+    shortcut = {
+      { desc = "[ Github]", group = "DashboardShortCut" },
+      { desc = "[ diegoulloao]", group = "DashboardShortCut" },
+    },
   },
-  {
-    icon = "  ",
-    desc = "Recently opened files                   ",
-    action = "Telescope oldfiles",
-    shortcut = "SPC f h",
-  },
-  {
-    icon = "  ",
-    desc = "Open Personal dotfiles                  ",
-    action = "edit ~/.config/nvim/lua/diegoulloao",
-    shortcut = "SPC f d",
-  },
-  {
-    icon = "  ",
-    desc = "Find File                               ",
-    action = "Telescope find_files find_command=rg,--hidden,--files",
-    shortcut = "SPC f f",
-  },
-  {
-    icon = "  ",
-    desc = "Find word                               ",
-    action = "Telescope live_grep",
-    shortcut = "SPC f w",
-  },
-  {
-    icon = "  ",
-    desc = "File Browser                            ",
-    action = "NvimTreeOpen",
-    shortcut = "SPC f b",
-  },
-}
+})
