63f2e9efd0e9b939620a5e34cdd030ccbfe23570
Diego <diegoulloao@icloud.com>
2023-07-15 14:32:55 -0400

    feat: autosync moved to another file

---
+++ autosync.lua
@@ -0,0 +1,25 @@
+-- packer setup
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[ packadd packer.nvim ]])
+    return true
+  end
+
+  return false
+end
+
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+-- autocommand that reloads neovim whenever you save this file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+return packer_bootstrap
--- plugins-setup.lua
+++ plugins-setup.lua
@@ -1,25 +1,5 @@
--- packer setup
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[ packadd packer.nvim ]])
-    return true
-  end
-
-  return false
-end
-
-local packer_bootstrap = ensure_packer() -- true if packer was just installed
--- autocommand that reloads neovim whenever you save this file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
-  augroup end
-]])
+-- require autosync when saving this file
+local packer_bootstrap = require("diegoulloao.autosync")
 
 -- call packer in safety mode
 local status, packer = pcall(require, "packer")
