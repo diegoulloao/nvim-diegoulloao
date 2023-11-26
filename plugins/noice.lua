6e4e738bc7e9f4f0b3089c692c2048f3f7568eaf
Diego <diegoulloao@icloud.com>
2023-11-25 21:11:29 -0300

    fix(temporary): noice view messages to false

--- plugins/noice.lua
+++ plugins/noice.lua
@@ -22,4 +22,7 @@ noice.setup({
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
     lsp_doc_border = false, -- add a border to hover docs and signature help
   },
+  messages = {
+    view = false,
+  },
 })
