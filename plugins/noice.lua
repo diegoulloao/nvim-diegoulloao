84448a740d5a55d637ce5a1b61d58ebacf625a32
Diego <diegoulloao@icloud.com>
2023-11-25 22:44:13 -0300

    fix(noice): cmdline vertical position

--- plugins/noice.lua
+++ plugins/noice.lua
@@ -7,19 +7,18 @@ end
 -- custom setup
 noice.setup({
   lsp = {
-    -- disable lsp progress / using fidget instead
+    -- disable lsp progress -> using fidget instead
     progress = {
       enabled = false,
     },
 
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    -- override markdown rendering so that **cmp** and other plugins use Treesitter
     override = {
       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
       ["vim.lsp.util.stylize_markdown"] = true,
       ["cmp.entry.get_documentation"] = true,
     },
   },
-  -- you can enable a preset for easier configuration
   presets = {
     bottom_search = true, -- use a classic bottom cmdline for search
     command_palette = true, -- position the cmdline and popupmenu together
@@ -28,6 +27,13 @@ noice.setup({
     lsp_doc_border = false, -- add a border to hover docs and signature help
   },
   messages = {
+    -- disable status line messages
     enabled = false,
+  },
+  cmdline = {
+    opts = {
+      -- vertically centered
+      position = "50%",
+    },
   },
 })
