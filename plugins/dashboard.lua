65f8352bf825393169262d4b7d955e88aacc57e5
Diego <diegoulloao@icloud.com>
2023-11-25 22:26:24 -0300

    fix(dashboard): ASCII art at header

--- plugins/dashboard.lua
+++ plugins/dashboard.lua
@@ -4,9 +4,23 @@ if not status then
   return
 end
 
+-- ASCII art header
+-- source: https://patorjk.com/software/taag/#p=display&f=Doom&t=diegoulloao
+local logo = [[
+     _ _                        _ _                   
+    | (_)                      | | |                  
+  __| |_  ___  __ _  ___  _   _| | | ___   __ _  ___  
+ / _` | |/ _ \/ _` |/ _ \| | | | | |/ _ \ / _` |/ _ \ 
+| (_| | |  __/ (_| | (_) | |_| | | | (_) | (_| | (_) |
+ \__,_|_|\___|\__, |\___/ \__,_|_|_|\___/ \__,_|\___/ 
+               __/ |                                  
+              |___/                                   
+]]
+
 -- custom config
 dashboard.setup({
   config = {
+    header = vim.split(string.rep("\n", 2) .. logo, "\n"),
     shortcut = {
       { desc = "[ Github]", group = "DashboardShortCut" },
       { desc = "[ diegoulloao]", group = "DashboardShortCut" },
