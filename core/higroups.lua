a520892fda84592e840bfebb0daea7bbb6a2e904
Diego <diegoulloao@icloud.com>
2023-11-27 17:16:20 -0300

    fix: hi groups from external function, ayu palette table

---
+++ core/higroups.lua
@@ -0,0 +1,63 @@
+--------
+-- hi groups helper function
+-- @param palette table
+-- @return nil
+local function hi_groups(palette)
+  -- alias for formatting
+  local f = string.format
+
+  -- line number
+  vim.cmd(f([[ hi! LineNr guifg=%s ]], palette.dark2))
+
+  -- comments color
+  vim.cmd(f([[ hi! Comment guifg=%s ]], palette.dimmed2))
+
+  -- general border color
+  vim.cmd(f([[ hi! FloatBorder guibg=NONE guifg=%s ]], palette.secondary))
+
+  -- indent blankline
+  vim.cmd(f([[ hi! IndentBlanklineIndent1 guifg=%s gui=nocombine ]], palette.dark2))
+
+  -- nvim-tree arrows color
+  vim.cmd(f([[ hi! NvimTreeIndentMarker guifg=%s ]], palette.dark2))
+  vim.cmd(f([[ hi! NvimTreeFolderArrowClosed guifg=%s gui=bold ]], palette.dimmed))
+  vim.cmd(f([[ hi! NvimTreeFolderArrowOpen guifg=%s gui=bold ]], palette.dimmed))
+
+  -- nvim-cmp custom colors
+  vim.cmd(f([[ hi! CursorLineBG guibg=%s guifg=%s ]], palette.dark, palette.light))
+
+  -- statusline msg area text color
+  vim.cmd(f([[ hi! MsgArea guifg=%s ]], palette.dimmed2))
+
+  -- nvim-cmp custom colors
+  vim.cmd(f([[ hi! BorderBG guibg=NONE guifg=%s ]], palette.primary))
+  vim.cmd(f([[ hi! CursorLineBG guibg=%s guifg=%s ]], palette.primary, palette.dark))
+
+  -- telescope custom colors
+  vim.cmd(f([[ hi! TelescopeNormal guifg=%s ]], palette.dimmed3))
+  vim.cmd(f([[ hi! TelescopePromptNormal guifg=%s ]], palette.secondary))
+  vim.cmd(f([[ hi! TelescopePromptBorder guifg=%s]], palette.primary))
+  vim.cmd(f([[ hi! TelescopeSelection guibg=%s guifg=%s ]], palette.dark3, palette.dimmed))
+  vim.cmd(f([[ hi! TelescopeSelectionCaret guifg=%s ]], palette.primary))
+  vim.cmd(f([[ hi! TelescopeMatching gui=bold guifg=%s ]], palette.primary))
+  vim.cmd(f([[ hi! TelescopePromptTitle guibg=%s guifg=%s ]], palette.primary, palette.dark))
+  vim.cmd(f([[ hi! TelescopeResultsTitle guibg=%s guifg=%s ]], palette.dimmed3, palette.dark))
+  vim.cmd(f([[ hi! TelescopeResultsBorder guifg=%s ]], palette.dark4))
+
+  -- lsp saga colors
+  vim.cmd(f([[ hi! SagaBorder guifg=%s]], palette.primary))
+
+  -- dashboard
+  vim.cmd(f([[ hi! DashboardHeader guifg=%s ]], palette.primary))
+  vim.cmd(f([[ hi! DashboardShortCut guifg=%s ]], palette.accent))
+  vim.cmd(f([[ hi! DashboardFooter guifg=%s ]], palette.primary))
+  vim.cmd(f([[ hi! DashboardFiles guifg=%s ]], palette.secondary))
+  vim.cmd(f([[ hi! DashboardRecentTitle guifg=%s ]], palette.dimmed4))
+  vim.cmd(f([[ hi! DashboardRecentProject guifg=%s ]], palette.dimmed4))
+  vim.cmd(f([[ hi! DashboardProjectIcon guifg=%s ]], palette.accent))
+
+  -- Treesitter context border bottom color
+  vim.cmd(f([[ hi! TreesitterContextSeparator gui=none guifg=%s ]], palette.highlight))
+end
+
+return hi_groups
--- themes/ayu.lua
+++ themes/ayu.lua
@@ -4,6 +4,46 @@ if not ayu_status then
   return
 end
 
+-- require highlight groups
+local hi_groups = require("diegoulloao.core.higroups")
+
+--[[
+  -- Palette reference: ayu 'dark'
+  #0F1419
+  #5C6773
+  #F07178
+  #FFEE99
+  #E7C547
+  #36A3D9
+  #95E6CB
+  #B8CC52
+  #FFB454
+  #E6B673
+  #FF7733
+
+  #FF3333
+  #F29718
+  #14191F
+  #2D3640
+]]
+
+-- general palette
+local palette = {
+  primary = "#FFB454",
+  secondary = "#59C2FF",
+  accent = "#C2D94C",
+  highlight = "#FF7733",
+  dark = "#000000",
+  dark2 = "#14191F",
+  dark3 = "#151A1E",
+  dark4 = "#253340",
+  dimmed = "#ACB4C2",
+  dimmed2 = "#393F4D",
+  dimmed3 = "#3E4B59",
+  dimmed4 = "#4D5566",
+  light = "#FFFFFF",
+}
+
 -- custom setup
 ayu.setup({
   mirage = false, -- set true to use 'mirage' version instead of 'dark'
@@ -12,72 +52,5 @@ ayu.setup({
 -- apply colorscheme
 ayu.colorscheme()
 
--- line number
-vim.cmd([[ hi! LineNr guifg=#14191F ]])
-
--- comments color
-vim.cmd([[ hi! Comment guifg=#393F4D ]])
-
--- indent blankline
-vim.cmd([[ hi! IndentBlanklineIndent1 guifg=#14191F gui=nocombine ]])
-
--- nvim-tree arrows color
-vim.cmd([[ hi! NvimTreeIndentMarker guifg=#14191F ]])
-vim.cmd([[ hi! NvimTreeFolderArrowClosed guifg=#acb4c2 gui=bold ]])
-vim.cmd([[ hi! NvimTreeFolderArrowOpen guifg=#acb4c2 gui=bold ]])
-
--- nvim-cmp custom colors
-vim.cmd([[ hi! CursorLineBG guibg=#000000 guifg=#FFFFFF ]])
-
--- statusline msg area text color
-vim.cmd([[ hi! MsgArea guifg=#393F4D ]])
-
--- nvim-cmp custom colors
-vim.cmd([[ hi! BorderBG guibg=NONE guifg=#FFB454 ]])
-vim.cmd([[ hi! CursorLineBG guibg=#FFB454 guifg=#000000 ]])
-
--- telescope custom colors
-vim.cmd([[ hi! TelescopeNormal guifg=#3E4B59 ]])
-vim.cmd([[ hi! TelescopePromptNormal guifg=#59C2FF ]])
-vim.cmd([[ hi! TelescopePromptBorder guifg=#FFB454]])
-vim.cmd([[ hi! TelescopeSelection guibg=#151A1E guifg=#acb4c2 ]])
-vim.cmd([[ hi! TelescopeSelectionCaret guifg=#FFB454 ]])
-vim.cmd([[ hi! TelescopeMatching gui=bold guifg=#FFB454 ]])
-vim.cmd([[ hi! TelescopePromptTitle guibg=#FFB454 guifg=#000000 ]])
-vim.cmd([[ hi! TelescopeResultsTitle guibg=#3E4B59 guifg=#000000 ]])
-vim.cmd([[ hi! TelescopeResultsBorder guifg=#253340 ]])
-
--- lsp saga colors
-vim.cmd([[ hi! SagaBorder guifg=#FFB454]])
-
--- dashboard
-vim.cmd([[ hi! DashboardHeader guifg=#FFB454 ]])
-vim.cmd([[ hi! DashboardShortCut guifg=#C2D94C ]])
-vim.cmd([[ hi! DashboardFooter guifg=#FFB454 ]])
-vim.cmd([[ hi! DashboardFiles guifg=#59C2FF ]])
-vim.cmd([[ hi! DashboardRecentTitle guifg=#4D5566 ]])
-vim.cmd([[ hi! DashboardRecentProject guifg=#4D5566 ]])
-vim.cmd([[ hi! DashboardProjectIcon guifg=#C2D94C ]])
-
--- Treesitter context border bottom color
-vim.cmd([[ hi! TreesitterContextSeparator gui=none guifg=#FF7733 ]])
-
---[[
--- Palette: ayu dark
-#0F1419
-#5C6773
-#F07178
-#FFEE99
-#E7C547
-#36A3D9
-#95E6CB
-#B8CC52
-#FFB454
-#E6B673
-#FF7733
-
-#FF3333
-#F29718
-#14191F
-#2D3640
-]]
+-- highlight groups
+hi_groups(palette)
