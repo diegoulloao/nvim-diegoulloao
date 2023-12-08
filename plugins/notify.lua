727cd9354eabef37126a4d5f2641dc513597a7f4
Diego <diegoulloao@icloud.com>
2023-12-08 18:59:04 -0300

    feat: notify now handles clean aspect in settings

--- plugins/notify.lua
+++ plugins/notify.lua
@@ -4,10 +4,19 @@ if not notify_status then
   return
 end
 
+-- require settings
+local settings = require("diegoulloao.settings")
+
+-- set simple render if aspect is clean
+local render = "default"
+if settings.aspect == "clean" then
+  render = "simple"
+end
+
 -- custom setup
 notify.setup({
   stages = "fade_in_slide_out", -- animation style
-  render = "default", -- notification appearance: default|simple
+  render = render, -- notification appearance: default|simple
   timeout = 3000, -- time to out
 })
 
