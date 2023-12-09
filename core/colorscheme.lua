cfc6209944b9c9a28af738892b265f3b332fd08c
Diego <diegoulloao@icloud.com>
2023-12-09 05:22:11 -0300

    fix(colorscheme): logic for use built-in nvim themes

--- core/colorscheme.lua
+++ core/colorscheme.lua
@@ -2,4 +2,9 @@
 local ACTIVE_THEME = require("diegoulloao.settings").theme
 
 -- require colorscheme
-require("diegoulloao.themes." .. ACTIVE_THEME)
+local status = pcall(require, "diegoulloao.themes." .. ACTIVE_THEME)
+
+-- set colorscheme if is a nvim built-in
+if not status then
+  vim.cmd(string.format([[ colorscheme %s ]], ACTIVE_THEME))
+end
