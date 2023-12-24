27efaeafda4a87695ddef0db383e57125b543683
Diego <diegoulloao@icloud.com>
2023-12-24 18:10:46 -0300

    fix(gruvbox): removed thick style

--- themes/gruvbox/init.lua
+++ themes/gruvbox/init.lua
@@ -7,9 +7,6 @@ end
 -- set dark background
 vim.opt.background = "dark"
 
--- options
-local widgets_style = "thick" -- clean|thick
-
 -- custom setup
 gruvbox.setup({
   italic = {
@@ -32,15 +29,8 @@ vim.cmd([[ hi! WinBarNC guibg=NONE ]])
 
 -- fix
 -- noice cmd line bg
-if widgets_style == "clean" then
-  vim.cmd([[ hi! NoiceCmdlinePopup guibg=#282828 ]])
-  vim.cmd([[ hi! NoiceCmdlinePopupBorder guibg=#282828 ]])
-else
-  vim.cmd([[ hi! NoiceCmdlinePopup guibg=#3c3836 ]])
-  vim.cmd([[ hi! NoiceCmdlinePopupBorder guibg=#3c3836 ]])
-  vim.cmd([[ hi! NoiceCmdlinePopupTitle guibg=#3c3836 ]])
-  vim.cmd([[ hi! NoiceCmdlineIcon guibg=#3c3836 ]])
-end
+vim.cmd([[ hi! NoiceCmdlinePopup guibg=#282828 ]])
+vim.cmd([[ hi! NoiceCmdlinePopupBorder guibg=#282828 ]])
 
 -- fix
 -- diagnostics sign bg
@@ -51,14 +41,7 @@ vim.cmd([[ hi! DiagnosticSignError guifg=#fb4934 guibg=#282828 ]])
 
 -- fix
 -- lsp saga bg
-if widgets_style == "clean" then
-  vim.cmd([[ hi! SagaNormal guibg=#282828 ]])
-else
-  vim.cmd([[ hi! SagaBorder guibg=#3c3836 ]])
-  vim.cmd([[ hi clear link SagaTitle Title ]])
-  vim.cmd([[ hi! SagaTitle guibg=#3c3836 ]])
-  -- vim.cmd([[ hi! Title guibg=#3c3836 ]]) -- do not change
-end
+vim.cmd([[ hi! SagaNormal guibg=#282828 ]])
 
 -- telescope
 vim.cmd([[ hi! TelescopeNormal guibg=#32302f ]])
