a56842dd94ac8db04d79136d954fe7f5d19dfda0
Diego <diegoulloao@icloud.com>
2023-12-29 22:55:04 -0300

    fix: diagnostics icons

--- core/config.lua
+++ core/config.lua
@@ -1,5 +1,5 @@
 -- set diagnostic signs
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = "󰛩 ", Info = " " }
 for type, icon in pairs(signs) do
   -- define diagnostic type
   local hl = "DiagnosticSign" .. type
