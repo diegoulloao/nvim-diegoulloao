bec7c5a51af5668ad4a5e9ae7ef51c69393f3e04
Diego <diegoulloao@icloud.com>
2024-01-07 03:43:22 -0300

    fix(ui): telescope prompt title

--- themes/colorschemes/gruvbox/init.lua
+++ themes/colorschemes/gruvbox/init.lua
@@ -64,7 +64,7 @@ return {
     vim.cmd([[ hi! TelescopeResultsBorder guibg=#32302f guifg=#32302f ]])
     vim.cmd([[ hi! TelescopePromptNormal guibg=#3c3836 ]])
     vim.cmd([[ hi! TelescopePromptBorder guibg=#3c3836 guifg=#3c3836 ]])
-    vim.cmd([[ hi! TelescopePromptTitle guibg=#d65d0e guifg=#3c3836 ]])
+    vim.cmd([[ hi! TelescopePromptTitle guibg=#d65d0e guifg=#3c3836 gui=bold ]])
     vim.cmd([[ hi! TelescopePromptPrefix guifg=#d65d0e ]])
     vim.cmd([[ hi! TelescopePromptCounter guifg=#83a598 ]])
     vim.cmd([[ hi! TelescopeSelection guifg=#d65d0e gui=bold ]])
