e5811c26e9f2d16cf72f888738100d66c49d2351
Diego <diegoulloao@icloud.com>
2024-06-02 22:48:09 -0400

    feat: zenmode

--- core/keymaps/main.lua
+++ core/keymaps/main.lua
@@ -83,5 +83,5 @@ keymap.set({ "n", "t" }, "<C-ç>", "<cmd>lua secondary_term_toggle()<CR>")
 keymap.set("n", "<leader>gf", "<cmd>OpenInGHFile<CR>") -- open current buffer in github
 keymap.set("v", "<leader>gf", "<cmd>OpenInGHFileLines<CR>") -- open current buffer in github + line
 
--- twilight
-keymap.set("n", "<leader>e", "<cmd>Twilight<CR>")
+-- zen mode
+keymap.set("n", "<leader>z", "<cmd>ZenMode<CR>")
---
+++ plugins/zenmode.lua
@@ -0,0 +1,28 @@
+return {
+  "folke/zen-mode.nvim",
+  cmd = { "ZenMode" },
+  opts = {
+    window = {
+      backdrop = 1,
+      width = 0.85,
+      options = {
+        signcolumn = "no",      -- disable signcolumn
+        number = false,         -- disable number column
+        relativenumber = false, -- disable relative numbers
+        cursorline = false,     -- disable cursorline
+        cursorcolumn = false,   -- disable cursor column
+        foldcolumn = "0",       -- disable fold column
+        list = false,           -- disable whitespace characters
+      },
+    },
+    options = {
+      laststatus = 0,
+    },
+    on_open = function()
+      vim.o.cmdheight = 1
+    end,
+    on_close = function()
+      vim.o.cmdheight = 0
+    end,
+  },
+}
