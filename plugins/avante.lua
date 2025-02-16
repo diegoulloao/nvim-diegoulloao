7a52eaf78c123b32e75fdaa31b9b92524c400874
Diego Ulloa <diegoulloao@icloud.com>
2025-02-16 17:23:36 -0300

    feat: avante for ask AI

---
+++ plugins/avante.lua
@@ -0,0 +1,61 @@
+return {
+  "yetone/avante.nvim",
+  -- enabled = false,
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  opts = {
+    provider = "openai",
+    openai = {
+      model = "gpt-4", -- your desired model (or use gpt-4o, etc.)
+    },
+    hints = { enabled = false },
+    mappings = {
+      submit = {
+        insert = "<C-CR>",
+      },
+    },
+    windows = {
+      width = 40,
+      sidebar_header = {
+        rounded = false,
+      },
+      input = {
+        height = 6,
+      },
+    },
+  },
+  build = "make",
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "echasnovski/mini.pick",
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+    "ibhagwan/fzf-lua",
+    "nvim-tree/nvim-web-devicons",
+    "zbirenbaum/copilot.lua",
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+        },
+      },
+    },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
